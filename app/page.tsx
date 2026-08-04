10fb424ba9377477d972b9f5b38de7efdb30e3c0
Vedanti Gawade <vgawade@xtsworld.in>
2026-08-04 12:51:40 +0530

    feat:create dashboard welcome page

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,16 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-       <h1>My learnind Dashboard</h1>
-       <p>Today I started Learning Next,js</p>
-       <button style={{backgroundColor: "red"}}>Get Started</button>
-      </main>
-    </div>
+    <main className="flex min-h-screen items-center justify-center bg-gray-100">
+      <div className="rounded-lg bg-white p-10 shadow-lg">
+        <h1 className="text-4xl font-bold text-blue-600">
+          Task Management Dashboard
+        </h1>
+
+        <p className="mt-4 text-gray-600">
+          Welcome! This project will help me learn React, Next.js, TypeScript, and GraphQL.
+        </p>
+      </div>
+    </main>
   );
 }
