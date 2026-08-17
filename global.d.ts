1b6d26ec6333b155ecadfb5214034b1961b8e713
Vedanti Gawade <vgawade@xtsworld.in>
2026-08-17 16:47:45 +0530

    update the navbar file name

---
+++ global.d.ts
@@ -0,0 +1,11 @@
+declare namespace JSX {
+  interface IntrinsicElements {
+    [elemName: string]: any;
+  }
+}
+
+declare module 'react/jsx-runtime' {
+  export function jsx(type: any, props?: any, key?: any): any;
+  export function jsxs(type: any, props?: any, key?: any): any;
+  export function jsxDEV(type: any, props?: any, key?: any): any;
+}
